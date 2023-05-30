7bd7bb0f08819cbe075805cd66d84546c8cac10f
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-30 10:03:29 -0500

    views-exercise completion

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,24 +1,38 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String allPosts() {
-        return "posts index page";
+    public String allPosts(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Title 1", "Body 1");
+        Post post2 = new Post("Title A", "Body A");
+        posts.add(post2);
+        posts.add(post1);
+
+        model.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost() {
-        return "view an individual post";
+    public String individualPost(Model model) {
+        Post post = new Post("The only post", "The only body");
+
+        model.addAttribute("post", post);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
