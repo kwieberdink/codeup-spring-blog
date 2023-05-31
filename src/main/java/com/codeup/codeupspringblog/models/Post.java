17d644ec70436ecb4555863726f2ff973a06b3ac
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-05-31 08:57:36 -0500

    repositories-exercises completion

--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -1,10 +1,28 @@
 package com.codeup.codeupspringblog.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length=100)
     private String title;
 
+    @Column(nullable = false, length=500)
     private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -22,7 +40,16 @@ public class Post {
         this.body = body;
     }
 
+    public Post() {
+    }
+
     public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
